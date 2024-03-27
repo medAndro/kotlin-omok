7a261983a00dcb2fb28eeae577317e1240edbfc3
woowahan-pjs <jason@woowahan.com>
2024-03-27 17:00:04 +0900

    feat: set up the android project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("com.android.application") version "8.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
 }
 
-group = "camp.nextstep.edu"
-version = "1.0-SNAPSHOT"
-
-kotlin {
-    jvmToolchain(21)
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.11.4")
-    testImplementation("org.assertj", "assertj-core", "3.27.3")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.9.1")
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
-    ktlint {
-        verbose.set(true)
-    }
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,16 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 rootProject.name = "kotlin-omok"
+include(":app")
